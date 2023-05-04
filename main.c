d7344202a832b2ac48fb85fe08d2fa39a60ec2b3
Ronan MEYER <ronanmeyergo@gmail.com>
2023-05-04 15:39:52 +0200

    La structure marche enfin

--- main.c
+++ main.c
@@ -13,30 +13,30 @@ typedef struct
     int depx, depy;
 }Pokemon, *Pok;
 
-Pok init_magicarpe();
+Pokemon init_magicarpe();
+Pokemon init_papillusion();
 void init_allegro();
 
 int main()
 {
+    //Initialisation Allegro
+    init_allegro();
+
     //Supposant qu'on prend les deux variables pari gagnant du joueur
     int pari_gagnant_j1 = 0;
     int pari_gagnant_j2 = 0;
 
-    //Autres variables
-    Pok Magicarpe;
+    //Autres variables + structures
+    Pokemon Magicarpe = init_magicarpe();
+    Pokemon Papillusion = init_papillusion();
     BITMAP * page = NULL;
-
-    //Initialisation Allegro + structures
-    init_allegro();
-    //Magicarpe = init_magicarpe();
-    BITMAP * test = load_bitmap("80x95-Magicarpe.bmp", NULL);
 
     //Double buffer
     page = create_bitmap(800, 600);
     clear_bitmap(page);
     if(!page) allegro_message("Erreur creation page");
 
-    draw_sprite(page, test, 0, 0);
+    draw_sprite(page, Magicarpe.im_1, 0, 0);
     blit(page,screen,0,0,0,0,SCREEN_W,SCREEN_H);
 
     //Boucle d'evenements
@@ -44,7 +44,7 @@ int main()
     {
         clear_bitmap(page);
         clear_to_color(page, makecol(255, 255, 255));
-        draw_sprite(page,test,0,0);
+        draw_sprite(page,Magicarpe.im_1,0,0);
         blit(page,screen,0, 0, 0, 0, SCREEN_W,SCREEN_H);
     }
 
@@ -52,32 +52,29 @@ int main()
     return 0;
 }END_OF_MAIN()
 
-Pok init_magicarpe()
+Pokemon init_magicarpe()
 {
-    Pokemon * Magicarpe = 0;
+    Pokemon Magicarpe =
+            {
+                    .im_1 = load_bitmap("80x95-Magicarpe.bmp",  NULL),
+                    .im_2 = load_bitmap("80x95-Magicarpe2.bmp",  NULL),
+                    .im_3 = load_bitmap("80x95-Magicarpe3.bmp", NULL),
+                    .im_4 = load_bitmap("80x95-Magicarpe4.bmp", NULL),
+                    .im_5 = load_bitmap("80x95-Magicarpe5.bmp", NULL),
+                    .tx = 0,
+                    .ty = 0,
+                    .posx = 0,
+                    .posy = 0,
+                    .depx = 0,
+                    .depy = 0
+            };
 
-    Magicarpe->im_1 = load_bitmap("80x95-Magicarpe.bmp",  NULL);
-    Magicarpe->im_2 = load_bitmap("80x95-Magicarpe2.bmp", NULL);
-    Magicarpe->im_3 = load_bitmap("80x95-Magicarpe3.bmp", NULL);
-    Magicarpe->im_4 = load_bitmap("80x95-Magicarpe4.bmp", NULL);
-    Magicarpe->im_5 = load_bitmap("80x95-Magicarpe5.bmp", NULL);
-
-    if(!Magicarpe->im_1 || !Magicarpe->im_2 || !Magicarpe->im_3 || !Magicarpe->im_4 || !Magicarpe->im_5)
+    if(!Magicarpe.im_1 || !Magicarpe.im_2 || !Magicarpe.im_3 || !Magicarpe.im_4 || !Magicarpe.im_5)
     {
         allegro_message("pb chargement image Magicarpe");
         allegro_exit();
         exit(EXIT_FAILURE);
     }
-
-    Magicarpe->tx = Magicarpe->im_1->w;
-    Magicarpe->tx = Magicarpe->im_1->h;
-
-    Magicarpe->posx = SCREEN_W/2-Magicarpe->tx/2;
-    Magicarpe->posy = SCREEN_H/2-Magicarpe->ty/2;
-
-    Magicarpe->depx = 9;
-    Magicarpe->depy = 5;
-
     return Magicarpe;
 }
 
