f6626ac5984b04a50b655883bc6b0803ab7b7037
Eleve <maximiliano.berte@edu.ece.fr>
2023-05-09 16:12:08 +0200

    V1

--- head.h
+++ head.h
@@ -2,6 +2,11 @@
 #define PROJET_HEAD_H
 #define NB_poke_guitare 5
 
+
+
+#define TRAINER_W 44
+#define TRAINER_H 46
+
 #include <stdio.h>
 #include "allegro.h"
 #include "time.h"
@@ -47,22 +52,16 @@ typedef struct liste{
     t_snake* head;
 }t_liste;
 
-bool bulle(t_player* player);
-bool snake_defeat(int nbpart,t_liste* serpent);
-void door_one(BITMAP* bit,t_player* player1);
 void snake();
-void afficherListe(t_liste *liste,BITMAP* page,BITMAP* head1,BITMAP* head2,BITMAP* head3,BITMAP* head4,int mouvement);
+void afficherListe(t_liste *liste,BITMAP* page);
 void pomme_init(BITMAP* base2,t_pomme* apple);
 bool colision_snake(t_liste* liste,t_pomme* pomme,BITMAP* base);
 t_liste *initialisation(BITMAP* base);
 void add_part(t_liste* liste,BITMAP* base);
 void move_snake(t_liste* serpent,int mouvement);
 
-
-
 ///////////jeux de matthias//////
 void deplacement(int y_poke[NB_poke_guitare], int vitesse,int *perreur);
 void verification_touche(int y_poke[NB_poke_guitare],int *perreur);
 void guitar_hero();
-int partie_guitar_hero(BITMAP* page);
 #endif //PROJET_HEAD_H
